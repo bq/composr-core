d5852ed8a5c952f0cde18ea7642799901e646b15
rmartinezcalvo <ricardo.martinez@bq.com>
2016-02-23 11:49:01 +0100

    new methods in virtualDomain added

--- src/lib/managers/VirtualDomain.js
+++ src/lib/managers/VirtualDomain.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var BaseManager = require('./base.manager.js');
 var virtualDomainValidator = require('../validators/virtualDomain.validator');
+var virtualDomainDao = require('../loaders/virtualDomainDao')
 
 var VirtualDomainManager = function (options) {
   this.events = options.events;
@@ -30,7 +31,7 @@ VirtualDomainManager.prototype._addToList = function (domain, vdModel) {
     this.__virtualDomains[domain] = {};
   }
 
-  this.__virtualDomains[domain] = vdModel;
+  this.__virtualDomains[domain][vModel.getApiId()] = vdModel;
 
   this.Phrases.register(this.domain, vdModel.getRawPhrases());
   this.Snippets.register(this.domain, vdModel.getRawSnippets());
@@ -68,6 +69,69 @@ VirtualDomainManager.prototype._getVirtualDomainsAsList = function () {
   return _.flatten(Object.keys(this.__phrases).map(function (key) {
     return module.__phrases[key];
   }));
+};
+
+VirtualDomainManager.prototype.getById = function(id) {
+    var domain = this._extracDomainFromId
+
+    if(this.__virtualDomains[domain][id] && _.size(this.__virtualDomains[domain][id]) > 0){
+        return this.__virtualDomains[domain][id]
+    } else {
+        return this.loadAndRegisterById(id)
+    }
+};
+
+VirtualDomainManager.prototype.getByDomain = function(domain) {
+    if(this.__virtualDomains[domain] && _.size(this.__virtualDomains[domain]) > 0){
+        return this.__virtualDomains[domain]
+    } else {
+        return this.loadAndRegisterByDomain(domain)
+    }
+};
+
+VirtualDomainManager.prototype.getAll = function() {
+    if(this.__virtualDomains && _.size(this.__virtualDomains) > 0){
+        return this.__virtualDomains
+    } else {
+        return this.loadAndRegisterAll()
+    }
+};
+
+VirtualDomainManager.prototype.loadAndRegisterById = function(id) {
+    var that = this
+    var vd
+    var domain = this._extracDomainFromId(id)
+
+    return virtualDomainDao.load(id)
+    .then(function(vd){
+        return that.getVirtualDomainModels(vd)
+    })
+    .then(function(vdModel){
+         that.events.emit('debug', 
+        'VirtualDomain', vmodel.getId(), 
+        'Phrases', vmodel.getRawPhrases().length,
+        'Snippets', vmodel.getRawSnippets().length);
+
+        return this._register(domain, vdModel)
+    })
+    .catch(function(err) {
+      that.events.emit('warn', 'data:error:loading');
+      return Promise.reject(err && err.data ? err.data : err);
+    });
+
+};
+
+VirtualDomainManager.prototype.loadAndRegisterByDomain = function(domain) {
+};
+
+VirtualDomainManager.prototype.loadAndRegisterAll = function() {
+};
+
+VirtualDomainManager.prototype.save = function(vd) {
+};
+
+VirtualDomainManager.prototype.validate = function(vd) {
+    return this.validate(vd)
 };
 
 module.exports = VirtualDomainManager;
